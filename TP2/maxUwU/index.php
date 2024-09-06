1c0521ff7c07fb00d536e75722ec36f31cf43f50
Maxime BOURCIEZ <maxime.bourciez@gmail.com>
2024-09-06 14:26:52 +0200

    Début PHP TP2

---
+++ TP2/maxUwU/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form>
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="name" required><br>
+
+        <label for="age">Age:</label>
+        <input type="number" id="age" name="age" required><br>
+
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" required><br>
+
+        <label for="donation">Donation Amount:</label>
+        <input type="number" id="donation" name="donation" required><br>
+
+        <input type="submit" value="Submit">
+    </form>
+
+    <?php
+
+    extract($POST, )
+
+    ?>
+</body>
+</html>
